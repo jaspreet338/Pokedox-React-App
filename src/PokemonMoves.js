ad2f6a4a66458c028736b40843acb5a116f5d09d
jaspreet kaur <kaurjas1928@yahoo.com>
2022-12-02 20:40:48 +0000

    changed if/else to terniary condition

--- src/PokemonMoves.js
+++ src/PokemonMoves.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from "react";
 
 function PokemonMoves() {
-    const [pokemonData, setPokemonData] = useState(null);
-    useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/1")
-        .then((res) => res.json())
-        .then((data)=>{
-            setPokemonData(data);
-            console.log(data);
-        });
-     },[]);
-  if (pokemonData) {
-    return (
-      <div>
-        <p>{pokemonData.name}'s moves:</p>
-        <ul>
-          {pokemonData.moves.map((move, index) => {
-            return <li key={index}>{move.move.name}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  } else {
-    return null;
-  }
+  const [pokemonData, setPokemonData] = useState(null);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemonData(data);
+        console.log(data);
+      });
+  }, []);
+  return (
+    <div>
+      {pokemonData ? (
+        <div>
+          <p>{pokemonData.name}'s moves:</p>
+          <ul>
+            {pokemonData.moves.map((move, index) => {
+              return <li key={index}>{move.move.name}</li>;
+            })}
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
 export default PokemonMoves;
