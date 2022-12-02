eb7e4e11a85bb75938cdebb30b2656fd7f5e3527
jaspreet kaur <kaurjas1928@yahoo.com>
2022-12-02 21:40:23 +0000

    updated caught-pokemon

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 const CaughtPokemon = (props) => {
-  const arrayPokemon = ["piko", "pika", "pike", "piki"];
-  const random = Math.floor(Math.random() * arrayPokemon.length);
+  //const arrayPokemon = ["piko", "pika", "pike", "piki"];
+  //const random = Math.floor(Math.random() * arrayPokemon.length);
   const [caught, setCaught] = useState([]);
-  const catchPokemon = () => {
-    setCaught(caught.concat(arrayPokemon[random]));
-  };
+  const[pokemonNameInput, setPokemonNameInput] = useState("");
+  function catchPokemon () {
+    setCaught(caught.concat(pokemonNameInput));
+  }
+  function handleInputChange(event){
+    setPokemonNameInput(event.target.value);
+  }
   return (
     <div>
+      <input type = "text" value = {pokemonNameInput} onChange = {handleInputChange}/>
       <p>
         Caught {caught.length} Pokemon on {props.date}
       </p>
